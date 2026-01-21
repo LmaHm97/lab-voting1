78bffcf0d8b4050dd4ad174c1edde080cc025510
LmaHm97 <112932524+LmaHm97@users.noreply.github.com>
2026-01-21 12:01:54 +0900

    Update api.py

--- lab_voting_backend/api.py
+++ lab_voting_backend/api.py
@@ -27,12 +27,7 @@ def create_app():
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
-    @app.before_request
-    def init_db_once():
-       if not getattr(app, "_db_initialized", False):
-          with app.app_context():
-               db.create_all()
-          app._db_initialized = True
+  
 
 
     # API
